6e1358b98e3a3b92096e0497275c11cf18cd9b4c
Ragnar Westad <ragnar.westad@gmail.com>
2024-10-12 20:29:38 +0200

    Lagt til spinener for lokasjonskomponenten

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   imports: [
     BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
 })
--- src/app/location-display/location-display.component.ts
+++ src/app/location-display/location-display.component.ts
@@ -1,13 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { NgIf } from '@angular/common';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-location-display',
-  template: '<p>{{ location }}</p>',
+  template: `
+    <div *ngIf="isLoading" class="location-container">
+      <mat-spinner diameter="40"></mat-spinner>
+      <div>Henter lokasjon ...</div>
+    </div>
+    <div *ngIf="!isLoading">
+      <p>{{ location }}</p>
+    </div>
+  `,
+  styles: ['.location-container { display: flex; flex-direction: column; align-items: center}'],
   standalone: true,
-  imports: [NgIf]
+  imports: [NgIf, MatProgressSpinnerModule]
 })
 export class LocationDisplayComponent implements OnInit {
+  isLoading = true;
   location: string = '';
 
   ngOnInit() {
@@ -18,9 +29,12 @@ export class LocationDisplayComponent implements OnInit {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((position) => {
         this.location = `Latitude: ${position.coords.latitude}, Longitude: ${position.coords.longitude}`;
+        this.isLoading = false;
+        console.log(this.location);
       });
     } else {
       this.location = 'Geolocation is not supported by this browser.';
+      this.isLoading = false;
     }
   }
 }
